04a3d0354c963093bedee3917c8a7c817ff78e14
JRgtech <35750543+JRgtech@users.noreply.github.com>
2018-09-27 17:47:42 -0500

    removed id

--- Javascript/confirm.js
+++ Javascript/confirm.js
@@ -1,6 +1,3 @@
-var pressed = document.getElementById('button');
-
-pressed.onclick = function userButton() {
-  getElementsByName('name');
-  alert("Thank you ")name(".A confirmation email will be sent out shortly");
+function userButton() {
+  alert ("Thank you\nA confirmation email will be sent out shortly");
 }
