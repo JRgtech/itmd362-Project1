c7577d4bde700dc2c6705c78af4f318de94231ed
JRgtech <35750543+JRgtech@users.noreply.github.com>
2018-09-27 17:31:37 -0500

    Added var and changed alert

--- Javascript/confirm.js
+++ Javascript/confirm.js
@@ -1,4 +1,6 @@
-function userButton() {
+var pressed = document.getElementById('button');
+
+button.onclick = function userButton() {
   getElementsByName('name');
-  alert.onclick("Thank you ")name(".A confirmation email will be sent out shortly");
+  alert("Thank you ")name(".A confirmation email will be sent out shortly");
 }
