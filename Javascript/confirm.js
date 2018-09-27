9cedb3e94689a807a57b7b6696b07fcceb083f8f
JRgtech <35750543+JRgtech@users.noreply.github.com>
2018-09-27 16:16:35 -0500

    Added missing function declaration

--- Javascript/confirm.js
+++ Javascript/confirm.js
@@ -1,4 +1,4 @@
-function userButton {
+function userButton() {
   getElementsByName('name');
-  alert("Thank you!" name "."" A confirmation email will be sent out shortly");
+  alert("Thank you" name ".A confirmation email will be sent out shortly");
 }
