af392830120dae020bd8c3421232dcd0514e7d55
Juan <Juan@Deez-Nutz.lan>
2018-10-31 21:59:39 -0500

    Create slideshow of images and added javascript

---
+++ javafldr/sldshw.js
@@ -0,0 +1,21 @@
+var imgcount = 1;
+imgCount(indx);
+
+function next(n) {
+  imgCount(indx += n);
+}
+
+function currentpic(n) {
+  imgCount(indx = n);
+}
+
+function imgCount(n) {
+  var i;
+  var pics = document.getElementsByClassName("shot");
+  if (n > pics.length) {indx = 1}
+  if (n < 1) {indx = pics.length}
+  for (i = 0; i < pics.length; i++) {
+    pics[i].style.display = "none";
+  }
+  pics[indx-1].style.display = "block";
+}
